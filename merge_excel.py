362a70c96852538f9d41cda46fb500d9a91bd0fc
Elia Wäfler <158746986+eliawaefler@users.noreply.github.com>
2024-07-24 16:44:16 +0200

    Add files via upload

---
+++ merge_excel.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# Excel-Datei und Blattnamen festlegen
+input_file = "Fachdatenmodell DIB.xls"
+input_file = "\\\\filer300\\USERS3004\\I0340828\\Desktop\\bessere_reports\\Fachdatenmodell DIB.xlsx"
+sheet_name = "Geschäftsobjekte"
+
+# Excel-Datei laden
+df = pd.read_excel(input_file, sheet_name=sheet_name, engine='xlrd')
+
+# Zeilen ab A3 (also ab Index 2) filtern, in denen die Spalte B den Text "Betriebsmittel" enthält
+filtered_df = df.iloc[2:]
+filtered_df = filtered_df[filtered_df['B'].str.contains("Betriebsmittel", na=False)]
+
+# Neue Excel-Datei speichern mit den gefilterten Daten der Spalte A
+output_file = "ISB_report.xlsx"
+filtered_df[['A']].to_excel(output_file, index=False, header=False)
+
+print(f"Die gefilterten Daten wurden in {output_file} gespeichert.")
