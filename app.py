471114a1fc9d64d20eaacd3dc52b0294a56f1ecd
Elia Wäfler <158746986+eliawaefler@users.noreply.github.com>
2024-07-29 09:14:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,10 @@ def download_datamodel(debug=False):
     Metadata Upload/Download | dataspot. Software Handbuch
     curl -X GET -u <user>:<password> "https://www.myserver.com/api/test/schemes/MyModel/download?xsl=<report>"
     """
-    path = "\\\\filer300\\USERS3004\\I0340828\\Desktop\\p_LAKEHUB\\prüfprozess_pyhton_lakehub"
 
     # Replace these with your actual username and password
-    username = st.secrets("USERNAME")
-    password = st.secrets("PW")
+    username = os.environ["USERNAME"]
+    password = os.environ["PW"]
     url = 'https://www.insel.dataspot.com/api/test/schemes/Kataloge/download?format=XLSX'
     url = "https://www.myserver.com/api/test/schemes/MyModel/download?format=XLSX"
     url = "https://insel.dataspot.io/api/test/schemes/MyModel/download?format=XLSX"
